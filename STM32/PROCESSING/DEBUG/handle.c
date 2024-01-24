f4a0dee7bf6d4acad43cdee7b3386b88a7156d1b
Nguyen Tuan Phong <85359726+NguyenTuanPhong2002@users.noreply.github.com>
2024-01-24 23:05:28 +0700

    Update handle.c

--- STM32/PROCESSING/DEBUG/handle.c
+++ STM32/PROCESSING/DEBUG/handle.c
@@ -10,12 +10,12 @@
 
 #include "string.h"
 
-extern UART_HandleTypeDef huart1;
+//extern UART_HandleTypeDef huart1;
 
 void print(const char *buffer)
 {
         char txBuffer[500];
         sprintf((char *)txBuffer, "%s\r\n", buffer);
         uint16_t len = strlen((char *)txBuffer);
-        HAL_UART_Transmit(&huart1, (uint8_t *)txBuffer, len, 100);
+        //HAL_UART_Transmit(&huart1, (uint8_t *)txBuffer, len, 100);
 }
